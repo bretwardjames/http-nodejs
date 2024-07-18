4c73dd35f83264b787b9f9d71ac63e9b2c1bbf54
bretwardjames <bretwardjames@gmail.com>
2024-07-18 11:26:00 -0600

    Initial setup and config

--- server.mjs
+++ server.mjs
@@ -1,6 +1,47 @@
-import { createServer } from 'http';
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const dotenv = require('dotenv');
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Middleware to handle API requests
+app.post('/proxy', async (req, res) => {
+  const { apiName, endpoint, method, data, headers } = req.body;
+
+  if (!apiName || !endpoint || !method) {
+    return res.status(400).send('Missing required parameters: apiName, endpoint, method');
+  }
+
+  const apiKey = process.env[`${apiName}_API_KEY`]
+  const apiUrl = process.env[`${apiName}_API_URL`]
+
+  const authHeader = apiName === 'KEAP' ? { 'X-KEAP-API-KEY': `${apiKey}` } : { 'Authorization': `Bearer ${apiKey}` }
+
+  try {
+    const response = await axios({
+      url: `${apiUrl}${endpoint}`,
+      method: method,
+      data: data,
+      headers: {
+        ...headers,
+        ...authHeader, // Add your API key here if required
+      },
+    });
+
+    res.status(response.status).send(response.data);
+  } catch (error) {
+    console.error('Error making API request:', error);
+    res.status(500).send('An error occurred while making the API request.');
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
