b1975d97e5e470705ae1cd1edccbba12ebb43593
bretwardjames <bretwardjames@gmail.com>
2024-07-23 12:33:06 -0600

    chore: Update URL parameters and display logic in calendar-scripts.js

--- calendar-scripts.js
+++ calendar-scripts.js
@@ -32,49 +32,50 @@ function getItemsWithPrefix(prefix) {
     return items;
 }
 
-document.addEventListener('DOMContentLoaded', function () {
 
-    // Replace + with %20
-    let updatedUrl = currentUrl.replace(/\+/g, '%20');
-    const urlParams = new URLSearchParams(window.location.search);
-    if (!urlParams.get('resources_to_invest') && !urlParams.get('Name') && !urlParams.get('Email') && !urlParams.get('household_income')) {
-        const localStorageItems = getItemsWithPrefix('submission_');
-        const baseUrl = getCurrentUrlWithoutParameters();
-        let shouldReload = false;
 
-        for (const key in localStorageItems) {
-            const shortKey = key.replace('submission_', '');
-            const value = localStorageItems[key];
-            if (value) {
-                urlParams.set(shortKey, value);
-                shouldReload = true;
-            }
-        }
+// Replace + with %20
+let updatedUrl = currentUrl.replace(/\+/g, '%20');
+const urlParams = new URLSearchParams(window.location.search);
+if (!urlParams.get('resources_to_invest') && !urlParams.get('Name') && !urlParams.get('Email') && !urlParams.get('household_income')) {
+    const localStorageItems = getItemsWithPrefix('submission_');
+    const baseUrl = getCurrentUrlWithoutParameters();
+    let shouldReload = false;
 
-        if (shouldReload) {
-            const newUrl = `${baseUrl}?${urlParams.toString()}`;
-            window.location.replace(newUrl);
+    for (const key in localStorageItems) {
+        const shortKey = key.replace('submission_', '');
+        const value = localStorageItems[key];
+        if (value) {
+            urlParams.set(shortKey, value);
+            shouldReload = true;
         }
     }
 
-    // Update the URL in the address bar without reloading the page
-    window.history.replaceState({}, document.title, updatedUrl);
-    const qualifiedSection = document.querySelector('[data-title="qualified"]');
-    const subSection = document.querySelector('[data-title="qualifiedSubSection"]');
-    const noSurveySection = document.querySelector('[data-title="noSurvey"]');
-    subSection.style.display = 'none';
-    qualifiedSection.style.display = 'none';
-    noSurveySection.style.display = 'none';
-    if (urlParams.get('resources_to_invest') && urlParams.get('Name') && urlParams.get('Email') && urlParams.get('household_income')) {
-        qualifiedSection.style.display = 'block';
-        subSection.style.display = 'block';
-    } else {
-        noSurveySection.style.display = 'block';
+    if (shouldReload) {
+        const newUrl = `${baseUrl}?${urlParams.toString()}`;
+        window.location.replace(newUrl);
     }
+}
 
-    const button = document.querySelector('[data-title="buttonSurvey"]')
-    button.querySelector('a').href += "?" + urlParams.toString()
-    console.log(button)
-});
+// Update the URL in the address bar without reloading the page
+window.history.replaceState({}, document.title, updatedUrl);
+const qualifiedSection = document.querySelector('[data-title="qualified"]');
+const subSection = document.querySelector('[data-title="qualifiedSubSection"]');
+const noSurveySection = document.querySelector('[data-title="noSurvey"]');
+subSection.style.display = 'none';
+qualifiedSection.style.display = 'none';
+noSurveySection.style.display = 'none';
+if (urlParams.get('resources_to_invest') && urlParams.get('Name') && urlParams.get('Email') && urlParams.get('household_income')) {
+    qualifiedSection.style.display = 'block';
+    subSection.style.display = 'block';
+} else {
+    noSurveySection.style.display = 'block';
+}
+
+const button = document.querySelector('[data-title="buttonSurvey"]')
+button.querySelector('a').href += "?" + urlParams.toString()
+console.log(button)
+
+
 
 
