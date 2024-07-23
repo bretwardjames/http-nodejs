29d75f3f64248049614dca223942f9645c8ec6d9
bretwardjames <bretwardjames@gmail.com>
2024-07-23 13:07:50 -0600

    Refactor form field handling in scripts.js for improved readability and performance

--- ple-scripts.js
+++ ple-scripts.js
@@ -1,18 +1,17 @@
-const today = new Date()
-const startingDate = 'March 21, 2025'
-const endingDate = 'March 23, 2025'
-const dateRange = 'March 21-23, 2025'
-const location = 'Kalari Resort in Round Rock, TX'
-const promoVideo = 'https://player.vimeo.com/video/354208410?muted=1&autoplay=1&&title=0&byline=0&wmode=transparent&autopause=0'
-const ticketGraphicMobile = ''
-const ticketGraphicDesktop = ''
-const compTicketGraphicMobile = ''
-const compTicketGraphicDesktop = ''
-const dateDate = isNaN(startingDate) ? null : new Date(startingDate)
-const eventYear = dateDate ? dateDate.getFullYear() : today.getMonth() < 3 ? today.getFullYear() : today.getFullYear() + 1
-
-
 document.addEventListener('DOMContentLoaded', () => {
+    const today = new Date()
+    const startingDate = 'March 21, 2025'
+    const endingDate = 'March 23, 2025'
+    const dateRange = 'March 21-23, 2025'
+    const location = 'Kalari Resort in Round Rock, TX'
+    const promoVideo = 'https://player.vimeo.com/video/354208410?muted=1&autoplay=1&&title=0&byline=0&wmode=transparent&autopause=0'
+    const ticketGraphicMobile = ''
+    const ticketGraphicDesktop = ''
+    const compTicketGraphicMobile = ''
+    const compTicketGraphicDesktop = ''
+    const dateDate = isNaN(startingDate) ? null : new Date(startingDate)
+    const eventYear = dateDate ? dateDate.getFullYear() : today.getMonth() < 3 ? today.getFullYear() : today.getFullYear() + 1
+
     const updateElements = (selector, content) => {
         document.querySelectorAll(selector).forEach(el => {
             const innerDiv = el.querySelector('.elHeadline');
